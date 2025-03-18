e61f8176847aa4aa813d9e8b238f20f5b0cf5c3c
Ikul23 <ikul23@icloud.com>
2025-03-18 15:16:08 +0300

    HT4 added in React App

---
+++ react-ht/src/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>О нас</h1>
+      <p>Здесь вы можете узнать больше о нашем проекте.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- react-ht/src/App.js
+++ react-ht/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
 import Message from './Message';
 import CommentsList from './CommentsList';
 import TemperatureConverter from './TemperatureConverter';
@@ -7,15 +10,44 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Message text="Привет, это мое первое сообщение в React!" />
-        <Message text="Это второе сообщение." />
-        <CommentsList />
-        <TemperatureConverter />
-        <TodoList />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          {/* Навигационное меню */}
+          <nav>
+            <ul style={{ listStyle: 'none', padding: 0 }}>
+              <li style={{ display: 'inline', marginRight: '20px' }}>
+                <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+                  Главная
+                </Link>
+              </li>
+              <li style={{ display: 'inline' }}>
+                <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>
+                  О нас
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* Маршруты */}
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Message text="Привет, это мое первое сообщение в React!" />
+                  <Message text="Это второе сообщение." />
+                  <CommentsList />
+                  <TemperatureConverter />
+                  <TodoList />
+                </>
+              }
+            />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
